2033a86a97194ecb5eedd1ff97ff807ef85a5255
omkardhuri123 <dhuri8973@gmail.com>
2025-07-16 21:07:56 +0530

    commit test

--- posts/tests.py
+++ posts/tests.py
@@ -3,26 +3,20 @@ from django.urls import reverse
 from .models import Post
 
 # Create your tests here.
-class PostTest(TestCase):
+class PostTests(TestCase):
     @classmethod
-    def setUpTestData(cls) -> None:
-        cls.post = Post.objects.create(text ="this is test method")
-    
+    def setUpTestData(cls):
+        cls.post = Post.objects.create(text='This is a test!')
+
     def test_model_content(self):
-        self.assertEqual(self.post.text, "this is test method")
-    
-    def test_url_exists_at_correct_location(self):  # new
+        self.assertEqual(self.post.text, 'This is a test!')
+
+    def test_url_exists_at_correct_location(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
-    def test_url_available_by_name(self):  # new
+    def test_homepage(self):  # new
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
-
-    def test_template_name_correct(self):  # new
-        response = self.client.get(reverse('home'))
         self.assertTemplateUsed(response, 'post_list.html')
-
-    def test_template_content(self):  # new
-        response = self.client.get(reverse('home'))
         self.assertContains(response, 'This is a test!')
